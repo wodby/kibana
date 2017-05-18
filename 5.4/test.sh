43fac770d80374419e5f7f9d233c939a170cbe08
Chingis Sandanov <chingis@wodby.com>
2017-05-18 13:25:42 +0700

    Set max_map_count for travis

--- 5.4/test.sh
+++ 5.4/test.sh
@@ -6,6 +6,10 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
+if [[ -n "${TRAVIS}" ]]; then
+    sudo sysctl -w vm.max_map_count=262144
+fi
+
 docker-compose up -d
 docker-compose exec elasticsearch make check-ready wait_seconds=5 max_try=12 delay_seconds=20 -f /usr/local/bin/actions.mk
 docker-compose exec kibana make check-ready wait_seconds=10 max_try=30 delay_seconds=60 -f /usr/local/bin/actions.mk
